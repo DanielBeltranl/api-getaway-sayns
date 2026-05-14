8e344cb80646a3d9b8aeb579fd09c12efc796326
Beltran Lastra <dbeltral@ext.sonda.com>
2026-05-13 22:23:21 -0400

    feat: Discovery service

--- src/main/java/cl/sys/discoveryservice/apigateawaysys2/config/SecurityConfig.java
+++ src/main/java/cl/sys/discoveryservice/apigateawaysys2/config/SecurityConfig.java
@@ -2,25 +2,33 @@ package cl.sys.discoveryservice.apigateawaysys2.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@Order(1) // Esto fuerza a que sea la primera configuración en cargar
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(csrf -> csrf.disable()) // OBLIGATORIO para el POST
                 .authorizeHttpRequests(auth -> auth
+                        // Usamos Strings directamente para evitar el error de la imagen image_d819fb.png
+                        .requestMatchers(HttpMethod.POST, "/api/registro/**").permitAll()
+                        .requestMatchers("/api/registro/**").permitAll()
                         .requestMatchers("/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(jwt -> {}) //
+                        .jwt(Customizer.withDefaults())
                 );
+
         return http.build();
     }
 }
